f1f178dfa970390a692490df0f7874d2e33fb63c
Jonathan <jonitoadriano@gmail.com>
2023-01-08 20:19:11 -0600

    solved array problem

--- arrays_select_practice_problem.js
+++ arrays_select_practice_problem.js
@@ -27,6 +27,18 @@ console.log(newArray);
 // #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 // #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
+var newArry = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}];
+var arry = [];
+
+newArry.forEach(function(key,value){
+if(key["price"] > 5){
+  arry.push(key);
+}
+
+});
+console.log(arry);
+
+
 // #  4. Start with an array of numbers and create a new array with only the even numbers.
 // #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
